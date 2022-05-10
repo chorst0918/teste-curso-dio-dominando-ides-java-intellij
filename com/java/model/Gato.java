97166abdbe22b756c3d61be79644c92f56b05494
chorst0918 <chorst0918@gmail.com>
2022-05-10 13:54:00 -0300

    teste intellij

--- com/java/model/Gato.java
+++ com/java/model/Gato.java
@@ -1,14 +1,7 @@
 package com.java.model;
 
+import br.com.dio.model.Gato;
+
 public class Gato {
 
-	private String nome;
-	private String cor;
-	private Integer idade;
-	
-	public Gato(String nome, String, cor, Integer idade){
-		this.nome= nome;
-		this.cor = cor;
-		this.idade = idade;
-	}
 }
